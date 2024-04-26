02f8d93fef7c4ea3646c271f5a2c27b7f98b32e4
Laurent Bristiel <lbristiel@prophesee.ai>
2024-04-26 15:11:33 +0200

    Change license

--- projects/kv260/srcs/sim_tc_001/hdl/tb_local_params.sv
+++ projects/kv260/srcs/sim_tc_001/hdl/tb_local_params.sv
@@ -1,6 +1,11 @@
-// Copyright (c) Prophesee S.A. - All Rights Reserved
-// Subject to Starter Kit Specific Terms and Conditions ("License T&C's").
-// You may not use this file except in compliance with these License T&C's.
+// Copyright (c) Prophesee S.A.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and limitations under the License.
 
 // Define here access to Zynq VIP instance
 `define ZYNQ_VIP_0 test_bench.kv260_top_wrapper.kv260_top_i.zynq_processing_system.inst
